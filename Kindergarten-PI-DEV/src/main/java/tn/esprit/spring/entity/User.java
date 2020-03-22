06b3e267b91f55cd11c3c2c5dce1954312ede4d0
Oussama <Oussama@DESKTOP-UO8LOF4>
2020-03-22 13:43:27 +0100

    attribut user

--- Kindergarten-PI-DEV/src/main/java/tn/esprit/spring/entity/User.java
+++ Kindergarten-PI-DEV/src/main/java/tn/esprit/spring/entity/User.java
@@ -2,9 +2,23 @@ package tn.esprit.spring.entity;
 
 import java.io.Serializable;
 
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
+@Table(name = "user")
 public class User implements Serializable {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String username;
+	private String password;
+	@Transient
+	private String passwordConfirm;
+	private Role roles;
 }
